54663c24b2e858c3982abb2b854002023e4dec6d
Robbie Bagley <rbagley01@gmail.com>
2023-06-30 11:53:29 -0700

    Added user api

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -10,7 +10,7 @@ from django.test import Client
 class AdminSiteTests(TestCase):
     """Test for Django admin."""
 
-    def setUp():
+    def setUp(self):
         """Create user and client."""
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
@@ -31,3 +31,19 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test the edit user page works."""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test the create user page works."""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+
